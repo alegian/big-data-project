c43d21e83cd7989d84e2cae331ddc8af612ab046
alegian <alegiannx@gmail.com>
2022-08-12 17:58:10 +0300

    fix bugs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from os.path import exists
@@ -75,9 +77,8 @@ def create_movie_ratings():
         .join(movie_df[['movieId', 'title']].set_index('movieId'), on='movieId')
 
     # rename for compatibility
-    movie_ratings_df = movie_ratings_df \
+    movie_ratings_df = movie_ratings_df.reset_index() \
         .rename(columns={
-            'rating': 'avg_rating',
             'movieId': 'movie_id',
             'title': 'movie_title',
             'userId': 'user_id',
@@ -87,7 +88,7 @@ def create_movie_ratings():
     # return the first 1000 rows, and create a CSV with the rest
     sample = movie_ratings_df.head(1000)
     if not exists('movie_ratings.csv'):
-        movie_ratings_df.drop(range(0, 1000))
+        movie_ratings_df = movie_ratings_df.tail(len(movie_ratings_df.index) - 1000)
         print('creating movie_ratings.csv...')
         movie_ratings_df.to_csv('movie_ratings.csv', index=False)
     return sample
@@ -110,7 +111,7 @@ def create_movie_details():
         .join(tag_df[['movieId', 'tag']].set_index('movieId'), on='movieId')
 
     # rename for compatibility
-    movie_details_df = movie_details_df \
+    movie_details_df = movie_details_df.reset_index() \
         .rename(columns={
             'rating': 'avg_rating',
             'movieId': 'movie_id',
@@ -121,7 +122,7 @@ def create_movie_details():
     # return the first 1000 rows, and create a CSV with the rest
     sample = movie_details_df.head(1000)
     if not exists('movie_details.csv'):
-        movie_details_df.drop(range(0, 1000))
+        movie_details_df = movie_details_df.tail(len(movie_details_df.index) - 1000)
         print('creating movie_details.csv...')
         movie_details_df.to_csv('movie_details.csv', index=False)
     return sample
@@ -136,7 +137,7 @@ def create_movie_genres():
     movie_genres_df['movie_year'] = movie_df['title'].str.slice(-5, -1)
 
     # rename for compatibility
-    movie_genres_df = movie_genres_df \
+    movie_genres_df = movie_genres_df.reset_index() \
         .rename(columns={
             'genres': 'movie_genres',
             'movieId': 'movie_id',
@@ -146,7 +147,7 @@ def create_movie_genres():
     # return the first 1000 rows, and create a CSV with the rest
     sample = movie_genres_df.head(1000)
     if not exists('movie_genres.csv'):
-        movie_genres_df.drop(range(0, 1000))
+        movie_genres_df = movie_genres_df.tail(len(movie_genres_df.index) - 1000)
         print('creating movie_genres.csv...')
         movie_genres_df.to_csv('movie_genres.csv', index=False)
     return sample
@@ -156,11 +157,15 @@ if __name__ == '__main__':
     session = db_connect()
     db_setup(session)
 
-    movie_ratings_sample = create_movie_ratings()
+    # movie_ratings_sample = create_movie_ratings()
     movie_details_sample = create_movie_details()
     movie_genres_sample = create_movie_genres()
 
-    insert_db(session, 'movie_ratings', movie_ratings_sample)
+    # fix dates
+    # movie_ratings_sample['rating_timestamp'] = movie_ratings_sample['rating_timestamp'] \
+    #     .apply(lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S'))
+
+    # insert_db(session, 'movie_ratings', movie_ratings_sample)
     insert_db(session, 'movie_details', movie_details_sample)
     insert_db(session, 'movie_genres', movie_genres_sample)
 
