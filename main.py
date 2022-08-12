2200752e0de818277fa3d99722aa0a9ca8128610
alegian <alegiannx@gmail.com>
2022-08-12 17:14:37 +0300

    insert 1000 rows in each table

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def db_connect():
 
 
 def db_setup(session):
-    # start = time.time()
     session.execute("""
     CREATE TABLE IF NOT EXISTS main.movie_ratings (
         movie_title text, 
@@ -50,88 +49,119 @@ def db_setup(session):
             PRIMARY KEY (movie_id)
         );
         """)
-    # end = time.time()
-    # print(end - start)
 
 
-def insert_movie_ratings(session):
-    rating_insert_query = session.prepare("""
-        INSERT INTO main.movie_ratings (user_id, movie_id, rating, rating_timestamp) 
-        VALUES (?, ?, ?, ?)
-    """)
-    # session.execute(rating_insert_query, [int(row[0]), int(row[1]), float(row[2]), datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S')])
+def insert_db(session, table_name, df):
+    start = time.time()
+
+    for row in df.values.tolist():
+        # variable table name, variable number of columns
+        insert_query = session.prepare(f"""
+            INSERT INTO main.{table_name} ({','.join(df.columns.values.tolist())}) 
+            VALUES ({','.join(['?']*len(df.columns))})
+        """)
+        session.execute(insert_query, row)
+
+    end = time.time()
+    print(f'Insert 1000 rows into {table_name} took {round(end-start, 4)} seconds')
 
 
-def create_movie_ratings_csv():
+def create_movie_ratings():
+    # load data
+    movie_df = pd.read_csv('movie.csv')
+    rating_df = pd.read_csv('rating.csv')
+    # generate table data
+    movie_ratings_df = rating_df\
+        .join(movie_df[['movieId', 'title']].set_index('movieId'), on='movieId')
+
+    # rename for compatibility
+    movie_ratings_df = movie_ratings_df \
+        .rename(columns={
+            'rating': 'avg_rating',
+            'movieId': 'movie_id',
+            'title': 'movie_title',
+            'userId': 'user_id',
+            'timestamp': 'rating_timestamp',
+        })
+
+    # return the first 1000 rows, and create a CSV with the rest
+    sample = movie_ratings_df.head(1000)
     if not exists('movie_ratings.csv'):
+        movie_ratings_df.drop(range(0, 1000))
         print('creating movie_ratings.csv...')
-        movie_df = pd.read_csv('movie.csv')
-        rating_df = pd.read_csv('rating.csv')
-
-        movie_ratings_df = rating_df\
-            .join(movie_df[['movieId', 'title']].set_index('movieId'), on='movieId')
-
-        movie_ratings_df \
-            .rename(columns={
-                'rating': 'avg_rating',
-                'movieId': 'movie_id',
-                'title': 'movie_title',
-                'userId': 'user_id',
-                'timestamp': 'rating_timestamp',
-            }) \
-            .to_csv('movie_ratings.csv', index=False)
+        movie_ratings_df.to_csv('movie_ratings.csv', index=False)
+    return sample
 
 
-def create_movie_details_csv():
+def create_movie_details():
+    # load data
+    movie_df = pd.read_csv('movie.csv')
+    rating_df = pd.read_csv('rating.csv')
+    tag_df = pd.read_csv('tag.csv')
+
+    # generate table data
+    avg_ratings_df = rating_df[['movieId', 'rating']] \
+        .groupby('movieId') \
+        .mean()\
+        .round({'rating': 2})
+    movie_details_df = avg_ratings_df \
+        .join(movie_df[['movieId', 'title', 'genres']].set_index('movieId'), on='movieId')
+    movie_details_df = movie_details_df \
+        .join(tag_df[['movieId', 'tag']].set_index('movieId'), on='movieId')
+
+    # rename for compatibility
+    movie_details_df = movie_details_df \
+        .rename(columns={
+            'rating': 'avg_rating',
+            'movieId': 'movie_id',
+            'title': 'movie_title',
+            'genres': 'movie_genres'
+        })
+
+    # return the first 1000 rows, and create a CSV with the rest
+    sample = movie_details_df.head(1000)
     if not exists('movie_details.csv'):
+        movie_details_df.drop(range(0, 1000))
         print('creating movie_details.csv...')
-        movie_df = pd.read_csv('movie.csv')
-        rating_df = pd.read_csv('rating.csv')
-        tag_df = pd.read_csv('tag.csv')
-
-        avg_ratings_df = rating_df[['movieId', 'rating']] \
-            .groupby('movieId') \
-            .mean()\
-            .round({'rating': 2})
-
-        movie_details_df = avg_ratings_df \
-            .join(movie_df[['movieId', 'title', 'genres']].set_index('movieId'), on='movieId')
-        movie_details_df = movie_details_df \
-            .join(tag_df[['movieId', 'tag']].set_index('movieId'), on='movieId')
-
-        movie_details_df\
-            .rename(columns={
-                'rating': 'avg_rating',
-                'movieId': 'movie_id',
-                'title': 'movie_title',
-                'genres': 'movie_genres'
-            })\
-            .to_csv('movie_details.csv', index=False)
+        movie_details_df.to_csv('movie_details.csv', index=False)
+    return sample
 
 
-def create_movie_genres_csv():
+def create_movie_genres():
+    # load data
+    movie_df = pd.read_csv('movie.csv')
+
+    # generate table data
+    movie_genres_df = movie_df.copy()
+    movie_genres_df['movie_year'] = movie_df['title'].str.slice(-5, -1)
+
+    # rename for compatibility
+    movie_genres_df = movie_genres_df \
+        .rename(columns={
+            'genres': 'movie_genres',
+            'movieId': 'movie_id',
+            'title': 'movie_title'
+         })
+
+    # return the first 1000 rows, and create a CSV with the rest
+    sample = movie_genres_df.head(1000)
     if not exists('movie_genres.csv'):
+        movie_genres_df.drop(range(0, 1000))
         print('creating movie_genres.csv...')
-        movie_df = pd.read_csv('movie.csv')
-
-        movie_genres_df = movie_df.copy()
-        movie_genres_df['movie_year'] = movie_df['title'].str.slice(-5, -1)
-
-        movie_genres_df \
-            .rename(columns={
-                'genres': 'movie_genres',
-                'movieId': 'movie_id',
-                'title': 'movie_title'
-            }) \
-            .to_csv('movie_genres.csv', index=False)
+        movie_genres_df.to_csv('movie_genres.csv', index=False)
+    return sample
 
 
 if __name__ == '__main__':
     session = db_connect()
     db_setup(session)
 
-    create_movie_ratings_csv()
-    create_movie_details_csv()
-    create_movie_genres_csv()
+    movie_ratings_sample = create_movie_ratings()
+    movie_details_sample = create_movie_details()
+    movie_genres_sample = create_movie_genres()
+
+    insert_db(session, 'movie_ratings', movie_ratings_sample)
+    insert_db(session, 'movie_details', movie_details_sample)
+    insert_db(session, 'movie_genres', movie_genres_sample)
 
     session.shutdown()
