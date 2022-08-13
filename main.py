782818e6ddeee30783585423c4f6dbba38e1d7a8
alegian <alegiannx@gmail.com>
2022-08-13 22:51:06 +0300

    fix many bugs & improve code

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ def db_setup(session):
             movie_genres text, 
             avg_rating float,
             tag text,
-            PRIMARY KEY ((movie_id, tag), avg_rating)
+            tag_user_id int,
+            PRIMARY KEY ((movie_id, tag_user_id, tag), avg_rating)
         ) WITH CLUSTERING ORDER BY (avg_rating DESC);
         """)
     session.execute("""
@@ -47,13 +48,14 @@ def db_setup(session):
             movie_title text, 
             movie_id int, 
             movie_genres text, 
-            movie_year text,
+            movie_year int,
             PRIMARY KEY (movie_id)
         );
         """)
 
 
 def insert_db(session, table_name, df):
+    print(f'Mass inserting 1000 rows into {table_name}. This might take some minutes...')
     start = time.time()
 
     for row in df.values.tolist():
@@ -87,6 +89,9 @@ def create_movie_ratings():
 
     # return the first 1000 rows, and create a CSV with the rest
     sample = movie_ratings_df.head(1000)
+    # fix dates
+    sample['rating_timestamp'] = sample['rating_timestamp'] \
+        .apply(lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S'))
     if not exists('movie_ratings.csv'):
         movie_ratings_df = movie_ratings_df.tail(len(movie_ratings_df.index) - 1000)
         print('creating movie_ratings.csv...')
@@ -108,7 +113,11 @@ def create_movie_details():
     movie_details_df = avg_ratings_df \
         .join(movie_df[['movieId', 'title', 'genres']].set_index('movieId'), on='movieId')
     movie_details_df = movie_details_df \
-        .join(tag_df[['movieId', 'tag']].set_index('movieId'), on='movieId')
+        .join(tag_df[['movieId', 'tag', 'userId']].set_index('movieId'), on='movieId')
+
+    # fix invalid user ids
+    movie_details_df.dropna(inplace=True)
+    movie_details_df.userId = movie_details_df.userId.astype(int)
 
     # rename for compatibility
     movie_details_df = movie_details_df.reset_index() \
@@ -116,7 +125,8 @@ def create_movie_details():
             'rating': 'avg_rating',
             'movieId': 'movie_id',
             'title': 'movie_title',
-            'genres': 'movie_genres'
+            'genres': 'movie_genres',
+            'userId': 'tag_user_id'
         })
 
     # return the first 1000 rows, and create a CSV with the rest
@@ -128,16 +138,30 @@ def create_movie_details():
     return sample
 
 
+# string hack to get movie year that appears in the form (1999) at the end of a string title
+def extract_year_from_title(title):
+    # last occurrence of '('
+    start_idx = title.rfind('(') + 1
+    # last occurrence of ')'
+    end_idx = title.rfind(')')
+
+    try:
+        return int(title[start_idx:end_idx])
+    except ValueError:
+        return 0
+
+
 def create_movie_genres():
     # load data
     movie_df = pd.read_csv('movie.csv')
 
     # generate table data
     movie_genres_df = movie_df.copy()
-    movie_genres_df['movie_year'] = movie_df['title'].str.slice(-5, -1)
+    # smart substring & typecast to extract year
+    movie_genres_df['movie_year'] = movie_df['title'].apply(extract_year_from_title)
 
     # rename for compatibility
-    movie_genres_df = movie_genres_df.reset_index() \
+    movie_genres_df = movie_genres_df \
         .rename(columns={
             'genres': 'movie_genres',
             'movieId': 'movie_id',
@@ -153,20 +177,21 @@ def create_movie_genres():
     return sample
 
 
+# creates data for all 3 tables, and inserts it all into CassandraDB
+def create_and_insert_data():
+    movie_ratings_sample = create_movie_ratings()
+    movie_details_sample = create_movie_details()
+    movie_genres_sample = create_movie_genres()
+
+    insert_db(session, 'movie_ratings', movie_ratings_sample)
+    insert_db(session, 'movie_details', movie_details_sample)
+    insert_db(session, 'movie_genres', movie_genres_sample)
+
+
 if __name__ == '__main__':
     session = db_connect()
     db_setup(session)
 
-    # movie_ratings_sample = create_movie_ratings()
-    movie_details_sample = create_movie_details()
-    movie_genres_sample = create_movie_genres()
-
-    # fix dates
-    # movie_ratings_sample['rating_timestamp'] = movie_ratings_sample['rating_timestamp'] \
-    #     .apply(lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S'))
-
-    # insert_db(session, 'movie_ratings', movie_ratings_sample)
-    insert_db(session, 'movie_details', movie_details_sample)
-    insert_db(session, 'movie_genres', movie_genres_sample)
+    create_and_insert_data()
 
     session.shutdown()
